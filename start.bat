d2628b2fbbedfd86fd06ab1a85710f83ac620e05
DerekCorniello <dcorniello.business@gmail.com>
2024-09-17 11:31:39 -0400

    we are so back pt 2

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
-@echo off
 echo Checking if Docker is running...
+@echo off
 docker info >nul 2>&1
 
 if %errorlevel% neq 0 (
@@ -9,5 +9,23 @@ if %errorlevel% neq 0 (
 )
 
 echo Starting Docker Compose services...
-docker-compose up --build --remove-orphans
-pause
+
+docker build -t frontend-image ./frontend
+docker build -t backend-image .
+
+docker run -d --name frontend-container -p 8081:8080 frontend-image
+docker run -d --name backend-container -p 8000:8000 backend-image
+
+echo Build Complete. Your app window will open soon.
+
+timeout /t 5 /nobreak >nul
+
+start "" http://localhost:8081
+
+echo App Started, press any key to close the app.
+echo You can minimize, but do not close, the window.
+
+pause >nul
+
+docker stop frontend-container backend-container
+docker rm frontend-container backend-container
